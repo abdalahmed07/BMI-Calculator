1d1a90a9759d668aa95c90ff7b5ce88d91485e9e
Abdal Ahmed <abdalahmed07@gmail.com>
2025-07-12 20:54:54 +0500

    Initial commit

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ function calculateBMI(event) {
 
   let category = "";
   if (bmi < 18.5) category = "Underweight";
-  else if (bmi < 24.9) category = "Normal";
-  else if (bmi < 29.9) category = "Overweight";
+  else if (bmi < 25) category = "Normal";
+  else if (bmi < 30) category = "Overweight";
   else category = "Obese";
 
   results.innerHTML = `<span>Your BMI is: <strong>${bmi}</strong> (${category})</span>`;
